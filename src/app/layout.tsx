57cefd01b8889ec3cdec4074e80ed5d531dcaf26
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2025-11-01 09:58:24 +0900

    feat: サイトのアイコンつけた

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,14 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Atlas, Inc",
   description: "We Are Atlas",
+  icons: {
+    icon: [
+      { url: "/favicon.png", type: "image/png", sizes: "512x512" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
